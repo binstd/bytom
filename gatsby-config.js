4274807e14b25161638095fb06a783f5ae32239c
ethluz67@126.com <lvxing>
2018-07-31 14:39:03 +0800

    添加提交到githubpage的功能
    添加提交到githubpage的功能

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@
 'use strict';
 
 module.exports = {
+  pathPrefix: "/bytom", 
   siteMetadata: {
     title: 'React: A JavaScript library for building user interfaces',
     siteUrl: 'https://reactjs.org',
